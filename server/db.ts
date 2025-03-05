4700d652d185d063c1b44de86e4fa26c2fd4065f
Amar Nafais <amarnafais@gmail.com>
2025-03-05 18:59:12 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -4,7 +4,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
+if (!process.env.DATABASE_URL) {
+    throw new Error(
+        'DATABASE_URL must be set. Did you forget to provision a database?'
+    );
+}
 
 export const pool = mysql.createPool(process.env.DATABASE_URL);
 
