2a36d5524c7aeda470913f8e1e7edb72338b2885
Amar Nafais <amarnafais@gmail.com>
2025-03-05 23:25:05 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -13,4 +13,4 @@ if (!process.env.DATABASE_URL) {
 export const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export const db = drizzle(pool);
-// changes made for above
+// changes test made for above
